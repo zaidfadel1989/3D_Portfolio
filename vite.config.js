95bdd5a18e461c956dc6e85f3397ad96140286c1
zaidfadel1989 <zaid.fadel89@gmail.com>
2025-11-16 00:19:02 -0600

    Setup4

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/3D_Portfolio/'  // must match your repo name
-})
+  base: '/3D_Portfolio/', // <-- add this line
+});
